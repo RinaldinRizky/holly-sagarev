9032249721f7bb5a04a4d6c21a40036279030fe5
Rinaldin Muhamad Rizky <rinaldinrizky@gmail.com>
2024-07-28 06:35:01 +0700

    1

--- js/app.js
+++ js/app.js
@@ -203,28 +203,28 @@ checkoutButton.addEventListener('click', async function(e){
 
 });
 
-getGoogleDriveLink(productID); {
-    const productDriveLinks = {
-        1: "https://drive.google.com/drive/folders/1zVcnn17eEY3ZRa451L4A0bZJw535ZLb0?usp=drive_link",
-        2: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        3: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        4: "https://drive.google.com/drive/folders/1qmprfU-AmgQzp0aKh0T6JiqWIQHCoKn8?usp=sharing",
-        5: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        6: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        7: "https://drive.google.com/drive/folders/1RQcSOMmdgN6Ne_p8XZ_M1E_7nuyb9B8M?usp=sharing",
-        8: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        9: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        10: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        11: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        12: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        13: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        14: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        15: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
-        31: "https://drive.google.com/drive/folders/1RXobWMQ3wevcn5BU8XcZyjyEhw4SBd-k?usp=sharing",
-    };
+// getGoogleDriveLink(productID); {
+//     const productDriveLinks = {
+//         1: "https://drive.google.com/drive/folders/1zVcnn17eEY3ZRa451L4A0bZJw535ZLb0?usp=drive_link",
+//         2: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         3: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         4: "https://drive.google.com/drive/folders/1qmprfU-AmgQzp0aKh0T6JiqWIQHCoKn8?usp=sharing",
+//         5: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         6: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         7: "https://drive.google.com/drive/folders/1RQcSOMmdgN6Ne_p8XZ_M1E_7nuyb9B8M?usp=sharing",
+//         8: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         9: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         10: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         11: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         12: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         13: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         14: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         15: "https://drive.google.com/drive/folders/1jDfEG1uUcPCdgk9QsVtSQEjYGchowK4p?usp=drive_link",
+//         31: "https://drive.google.com/drive/folders/1RXobWMQ3wevcn5BU8XcZyjyEhw4SBd-k?usp=sharing",
+//     };
 
-    return productDriveLinks[productID] || "https://drive.google.com/drive/folders/1jAvBCUlJbO-n3dhyNeRN7syjL9xYXyrR?usp=drive_link";
-};
+//     return productDriveLinks[productID] || "https://drive.google.com/drive/folders/1jAvBCUlJbO-n3dhyNeRN7syjL9xYXyrR?usp=drive_link";
+// };
 
 // contoh pesan whatsapp 
 const formatMessage = (obj) => {
