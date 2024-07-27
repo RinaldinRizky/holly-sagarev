84bb619be4876ba75895e5feae1b9ab21015ba9b
Rinaldin Muhamad Rizky <rinaldinrizky@gmail.com>
2024-07-28 00:57:57 +0700

    1

--- index.php
+++ index.php
@@ -117,14 +117,14 @@
 														<input type= "hidden" name="items" x-model="JSON.stringify($store.cart.items)">
 														<input type= "hidden" name="total" x-model="$store.cart.total">
 														<h5>Customer Detail</h5>
-														<!-- <label for="name">
+														<label for="name">
 															<span>Name</span>
 															<input type="text" name="name" id="name"> 
 														</label>
 														<label for="email">
 															<span>Email</span>
 															<input type="email" name="email" id="email"> 
-														</label> -->
+														</label>
 														<div class="cart-bottom  clearfix">
 															<button href="#" class="button-one floatright text-uppercase" data-text="Check out">Check out</button>
 														</div>
