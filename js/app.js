15fa8560eb6b0c983247b3f887c6450fe9ffb0f7
Rinaldin Muhamad Rizky <rinaldinrizky@gmail.com>
2024-07-28 01:04:52 +0700

    1

--- js/app.js
+++ js/app.js
@@ -123,32 +123,32 @@ document.addEventListener('alpine:init', () => {
     }));
 });
 
-// Form Validation
-const checkoutButton = document.querySelector('.button-one');
-checkoutButton.disabled = true;
+// // Form Validation
+// const checkoutButton = document.querySelector('.button-one');
+// checkoutButton.disabled = true;
 
-const form = document.querySelector('#checkoutForm');
+// const form = document.querySelector('#checkoutForm');
 
-form.addEventListener('input', function() {
-    let allFilled = true;
+// form.addEventListener('input', function() {
+//     let allFilled = true;
 
-    for (let i = 0; i < form.elements.length; i++) {
-        if (form.elements[i].type !== 'button' && form.elements[i].type !== 'submit' && !form.elements[i].readOnly && form.elements[i].type !== 'hidden') {
-            if (form.elements[i].value.trim() === '') {
-                allFilled = false;
-                break;
-            }
-        }
-    }
+//     for (let i = 0; i < form.elements.length; i++) {
+//         if (form.elements[i].type !== 'button' && form.elements[i].type !== 'submit' && !form.elements[i].readOnly && form.elements[i].type !== 'hidden') {
+//             if (form.elements[i].value.trim() === '') {
+//                 allFilled = false;
+//                 break;
+//             }
+//         }
+//     }
 
-    if (allFilled) {
-        checkoutButton.disabled = false;
-        checkoutButton.classList.remove('disabled');
-    } else {
-        checkoutButton.disabled = true;
-        checkoutButton.classList.add('disabled');
-    }
-});
+//     if (allFilled) {
+//         checkoutButton.disabled = false;
+//         checkoutButton.classList.remove('disabled');
+//     } else {
+//         checkoutButton.disabled = true;
+//         checkoutButton.classList.add('disabled');
+//     }
+// });
 
 
 // Kirim Data ketika tombol checkout di klik
